c7e2ca7d404317516357c4997abca3bbfabd82bf
Samed Karakus <karakusnavy@gmail.com>
2018-12-12 15:39:23 +0300

    updated

---
+++ MvcViewData/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcViewData.Models;
+
+namespace MvcViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("")]
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        [Route("info/{id}/{name}")]
+        public ActionResult Panel(int id,string name)
+        {
+            List<string> persons = new List<string>();
+            persons.Add("Naber 1");
+            persons.Add("Naber 2");
+            persons.Add("Naber 3");
+            persons.Add("Naber 4");
+            persons.Add("Naber 5");
+            var bilgiler = new info()
+            {
+                personname = name,
+                personid = id,                        
+                persons = persons
+            };
+            return View(bilgiler);
+        }
+    }
+}
---
+++ MvcViewData/Models/info.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcViewData.Models
+{
+    public class info
+    {
+        public int personid { get; set; }
+        public string personname { get; set; }
+        public List<string> persons { get; set; }
+    }
+}
